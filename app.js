21e9cd3b6d12d6ad7b18a8370a24a1b4a86f3058
Ethan Choi <ethanchoi.812@gmail.com>
2017-01-25 13:32:48 +0800

    Update 3
    Updated display for operators, separate functions for operators,
    updated css, multiply decimal issue

--- app.js
+++ app.js
@@ -1,40 +1,79 @@
 
 var result = document.getElementById('result');
 
-var btnObj = {
-	"one" : '1',
-	"two" : '2',
-	"three" : '3',
-	"four" : '4',
-	"five" : '5',
-	"six" : '6',
-	"seven" : '7',
-	"eight" : '8',
-	"nine" : '9',
-	"zero" : '0',
-	"plus" : '&#43;',
-	"minus" : '&#45;',
-	"multiply" : '&#42;',
-	"divide" : '&#47'
+var numObj = {
+	'one' : '1',
+	'two' : '2',
+	'three' : '3',
+	'four' : '4',
+	'five' : '5',
+	'six' : '6',
+	'seven' : '7',
+	'eight' : '8',
+	'nine' : '9',
+	'zero' : '0',
+	
 }
 
-var elements = document.getElementsByClassName('btn');
-
-for (var i = 0; i < elements.length; i++) {
-    elements[i].addEventListener('click', function(){numFunction(this.id)}, false);
+var opObj = {
+	'plus' : ['&#43;','+'],
+	'minus' : ['&minus;','-'],
+	'multiply' : ['&times;', '*'],
+	'divide' : ['&div;', '/'],
+	'decimal' : ['&period;', '.']
 }
+
+var numElements = document.getElementsByClassName('num-btn');
+
+for (var i = 0; i < numElements.length; i++) {
+    numElements[i].addEventListener('click', function(){
+    	numFunction(this.id);
+    	
+    }, false);
+}
+
+var opElements = document.getElementsByClassName('op-btn');
+
+for (var i = 0; i < opElements.length; i++) {
+    opElements[i].addEventListener('click', function(){
+    	opFunction(this.id);
+    }, false);
+}
+
+document.getElementById('equals').onclick = equalFunction;
+document.getElementById('del').onclick = delFunction;
+document.getElementById('clear').onclick = clearFunction;
+
+var acct="";
 
 function numFunction(clickedId){
 //get value of btnObject based on btn ID
-	result.innerHTML += btnObj[clickedId];
+	result.innerHTML += numObj[clickedId];
+	acct += numObj[clickedId];
+	acct.toString();
+}
+
+function opFunction(clickedId){
+//get value of btnObject based on btn ID
+	result.innerHTML += opObj[clickedId][0];
+	acct += opObj[clickedId][1];
+}
+
+function equalFunction(e){
+	var total = eval(acct);
+	result.innerHTML = Math.round(total*1000) / 1000;
+	acct = Math.round(total*1000 / 1000);
 }
 
 function clearFunction(){
-//get value of btnObject based on btn ID
+//clear result div
 	result.innerHTML = '';
+	acct = "";
 }
 
-function equalFunction(){
-	result.innerHTML = eval(result.innerHTML);
+function delFunction(){
+//remove last character
+	result.innerHTML = (result.innerHTML).slice(0, -1);
+	acct = acct.slice(0, -1);
 }
 
